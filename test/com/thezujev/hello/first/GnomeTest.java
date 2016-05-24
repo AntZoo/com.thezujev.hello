b53c45942e202da6598ddf185227e9b4bd604fb0
antzoo <anton@zujev.eu>
2016-05-24 16:56:12 +0200

    Added gnometest

---
+++ test/com/thezujev/hello/first/GnomeTest.java
@@ -0,0 +1,52 @@
+package com.thezujev.hello.first;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zujevant on 24.5.2016.
+ */
+public class GnomeTest {
+
+    private static Gnome gnome;
+
+    @BeforeClass
+    public static void OneTimeSetup() {
+        gnome = new Gnome();
+    }
+
+    @Test
+    public void getRandomNumber() throws Exception {
+        assertEquals(gnome.randomNumber, gnome.getRandomNumber());
+    }
+
+    @Test
+    public void getPredefinedRandomNumber() throws Exception {
+        gnome.randomNumber = 55;
+        assertEquals("My random number is 55.", gnome.tellRandomNumber());
+        gnome.generateRandomNumber();
+    }
+
+    @Test
+    public void greet() throws Exception {
+        assertEquals("Hello World!", gnome.greet());
+    }
+
+    @Test
+    public void tellRandomNumber() throws Exception {
+        assertEquals("My random number is " + gnome.randomNumber + ".", gnome.tellRandomNumber());
+    }
+
+    @Test
+    public void drawOneLevelChristmasTree() throws Exception {
+        assertEquals("*\n*", gnome.drawChristmasTree(1));
+    }
+
+    @Test
+    public void drawFiveLevelChristmasTree() throws Exception {
+        assertEquals("    *\n   ***\n  *****\n *******\n*********\n    *", gnome.drawChristmasTree(5));
+    }
+}
