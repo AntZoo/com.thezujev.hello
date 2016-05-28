6a5685c572f52d79a978178d7512e1f0e1045354
Anton Zujev <anton.zujev@tieto.com>
2016-05-29 00:00:41 +0200

    working version

--- src/com/thezujev/hello/third/Palindrome.java
+++ src/com/thezujev/hello/third/Palindrome.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * Created by zujevant on 28-May-16.
  */
 public class Palindrome {
-    private Scanner scan;
+    private Scanner scan = new Scanner(System.in);
 
     public void runPalindrome() {
         System.out.println("This method will decide whether the string you send is a palindrome or not.");
@@ -19,14 +19,14 @@ public class Palindrome {
     }
 
     private void solvePalindrome(String input) {
-        input = input.replaceAll("\\W", "");
+        input = input.replaceAll("\\W", "").toLowerCase();
 
         boolean easy = easySolution(input);
         boolean recursive = recursiveSolution(input);
 
         System.out.println("----------------------------------------");
-        System.out.println("The easy solution says :" + (easy ? "YES, it is a palindrome!" : "NO, it is not a palindrome."));
-        System.out.println("The recursive solution says :" + (recursive ? "YES, it is a palindrome!" : "NO, it is not a palindrome."));
+        System.out.println("The easy solution says: " + (easy ? "YES, it is a palindrome!" : "NO, it is not a palindrome."));
+        System.out.println("The recursive solution says: " + (recursive ? "YES, it is a palindrome!" : "NO, it is not a palindrome."));
     }
 
     protected boolean easySolution(String input) {
@@ -36,6 +36,10 @@ public class Palindrome {
     }
 
     protected boolean recursiveSolution(String input) {
-        return (input.charAt(0) == input.charAt(input.length()-1) && recursiveSolution(input.substring(1, input.length()-1)));
+        if (input.length() > 1) {
+            return (input.charAt(0) == input.charAt(input.length() - 1) && recursiveSolution(input.substring(1, input.length() - 1)));
+        } else {
+            return true;
+        }
     }
 }
